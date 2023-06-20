62ae283ef744a693d2844601851694dfd0494406
daphnecor <cor.daphne@gmail.com>
2023-06-20 16:03:27 -0400

    Update

--- experiments/human_regularized/constants.py
+++ experiments/human_regularized/constants.py
@@ -7,9 +7,9 @@ class PPOExperimentConfig:
     seed: int = 1                     # seed of the experiment
     torch_deterministic = True        # if toggled, `torch.backends.cudnn.deterministic=False`
     cuda = False                      # if toggled, cuda will be enabled by default
-    total_iters: int = 5000           # total iterations of the experiments
-    num_policy_rollouts: int = 20     # determines the batch size, the amount of experience to collect before doing an optim step
-    num_steps: int = 80               # the number of steps to run in each environment per policy rollout
+    total_iters: int = 50             # total iterations of the experiments
+    num_policy_rollouts: int = 70     # determines the batch size, the amount of experience to collect before doing an optim step
+    num_steps: int = 90               # the number of steps to run in each environment per policy rollout
     learning_rate: float = 2.5e-4     # the learning rate of the optimizer 
     anneal_lr: float = True           # toggle learning rate annealing for policy and value networks
     gamma: float = 0.99               # the discount factor gamma
@@ -27,10 +27,20 @@ class PPOExperimentConfig:
 @dataclass
 class NocturneConfig:
     env_id: str = 'Nocturne-v0'
-    nocturne_rl_cfg: str = '/scratch/dc4971/nocturne/experiments/human_regularized/rl_config.yaml'
+    nocturne_rl_cfg: str = 'experiments/human_regularized/rl_config.yaml'
+    take_random_actions: bool = False
     train_data_dir: str = None
     valid_data_dir: str = None
     test_split: float = None
+
+@dataclass
+class WandBSettings:
+    track: bool = True
+    record_video: bool = True
+    fps: int = 5
+    project_name: str = 'human_regularized_rl'
+    group: str = 'nocturne'
+    exp_name: str = 'vid_test_ppo_agent'
 
 @dataclass
 class HumanPolicyConfig:
@@ -39,12 +49,4 @@ class HumanPolicyConfig:
     actions_discretizations = [15, 42]
     actions_bounds = [[-6, 6], [-0.7, 0.7]]  # Bounds for (acc, steering)
     lr: float = 1e-4
-    pretrained_model_path: str = '/scratch/dc4971/nocturne/experiments/human_regularized/human_anchor_policy_AS.pth'
-
-@dataclass
-class WandBSettings:
-    track: bool = False
-    project_name: str = 'human_regularized_rl'
-    group: str = 'nocturne'
-    exp_name: str = 'ppo_large_batch'
-
+    pretrained_model_path: str = 'experiments/human_regularized/human_anchor_policy_AS.pth'
