05ef795f8506bb2601a418010958021c02647d02
daphnecor <cor.daphne@gmail.com>
2023-07-06 06:29:30 -0400

    minor changes

--- experiments/human_regularized/pool_test.py
+++ experiments/human_regularized/pool_test.py
@@ -3,23 +3,53 @@ from itertools import repeat
 from multiprocessing import Pool
 import os
 import time
+from nocturne import Action
 
 from base_env import BaseEnv
 from rl_models import Agent
 import torch
 import utils
+import numpy as np
 
 my_value = None
     
 def f(x, Env, args_rl_env, agent):
     time.sleep(2)
     env = Env(args_rl_env)
-    env.reset()
+    next_obs_dict = env.reset()
 
-    agent.get_action_and_value(torch.zeros(size=(5, )))
+    moving_vehs = env.scenario.getObjectsThatMoved()
 
-    for step in range(10):
-        env.step()
+    # Set data buffer for within scene logging
+    rollout_buffer = utils.RolloutBuffer(
+        [agent.id for agent in moving_vehs],
+        80,
+        env.observation_space.shape[0],
+        env.action_space.n,
+        "cpu", # args_exp.device,
+    )
+    
+    for timestep in range(80):
+
+        # # # #  Interact with environment  # # # #
+        next_obs_dict, reward_dict, next_done_dict, info_dict = env.step({
+            veh.id: Action(acceleration=2.0, steering=1.0, head_angle=0.5)
+            for veh in moving_vehs
+        })
+
+        with torch.no_grad():
+            action = agent.get_action_and_value(torch.zeros(size=(5, )))
+
+        # Update tensors
+        for veh in moving_vehs:
+            agent_id = veh.id
+            rollout_buffer.rewards[agent_id][timestep] = torch.ones(size=(1,))
+
+
+        
+
+    return rollout_buffer
+
 
 if __name__ == '__main__':
     
@@ -35,12 +65,12 @@ if __name__ == '__main__':
 
     start_time = time.time()
     for x in range(NUM_TASKS):
-        f(x, BaseEnv, args_rl_env, deepcopy(ppo_agent))
+        rollout_buffers = f(x, BaseEnv, args_rl_env, deepcopy(ppo_agent))
     end_time = time.time()
     print(f"Duration w/o Pool: {end_time - start_time}")
     
     with Pool(processes=NUM_PROCESSES) as pool:
         start_time = time.time()
-        pool.starmap(f, zip(range(NUM_TASKS), repeat(BaseEnv), repeat(args_rl_env), repeat(deepcopy(ppo_agent))))
+        rollout_buffers = pool.starmap(f, zip(range(NUM_TASKS), repeat(BaseEnv), repeat(args_rl_env), repeat(deepcopy(ppo_agent))))
         end_time = time.time()
         print(f"Duration w/ Pool: {end_time - start_time}")
